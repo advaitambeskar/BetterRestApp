48331a2cd0d3ca7cc52f3d94fa91dd92983c8282
Advait Ambeskar <ambeskaradvait@gmail.com>
2021-10-13 19:20:02 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Advait Ambeskar on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
