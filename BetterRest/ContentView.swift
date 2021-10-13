beb8fb1e3cf98ca4ac89fc331d6ab61f153fc5cd
Advait Ambeskar <ambeskaradvait@gmail.com>
2021-10-13 22:56:43 +0530

    Project end

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -7,10 +7,95 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct largeText: View {
+    var textValue: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(textValue)
+            .font(.largeTitle)
             .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    let model = BetterRest()
+    
+    
+    func calculateBedtime() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is…"
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .datePickerStyle(WheelDatePickerStyle())
+                
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Picker("Amount of Coffee", selection: $coffeeAmount) {
+                        ForEach(0 ..< 20) { amount in
+                            Text("\(amount)")
+                        }
+                    }
+                }
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: largeText(textValue: alertMessage), dismissButton: .default(Text("OK")))
+            }
+            
+            .navigationBarTitle("Better Rest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+            })
+        }
     }
 }
 
